849f383b08d663b8e44fc102271670c48d24cde6
Ivan <iv.menshev2003@gmail.com>
2024-11-19 19:09:41 +0300

    Начало работы
    ы

---
+++ src/main/java/com/example/Air_transport/views/MainView.java
@@ -0,0 +1,9 @@
+package com.example.Air_transport.views;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends Composite<VerticalLayout> {
+}
