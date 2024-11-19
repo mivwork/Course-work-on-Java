70fb44aeb1556afa75a9b2a95698b75b61ddc9ac
Ivan <iv.menshev2003@gmail.com>
2024-11-19 19:00:23 +0300

    Создание проекта

---
+++ src/main/java/com/example/Air_transport/AirTransportApplication.java
@@ -0,0 +1,13 @@
+package com.example.Air_transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Air_transport/AirTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Air_transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
